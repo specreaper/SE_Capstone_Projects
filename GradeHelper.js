b0ac4aed7d06d0b124a7049f2a07d1a2b65f54d3
Sean Evans <sean.evans1.2020@gmail.com>
2025-09-05 07:49:36 -0400

    GradeHelper.js

--- GradeHelper.js
+++ GradeHelper.js
@@ -1,3 +1,15 @@
+async function saveInput() {
+  	// Grab the value from the text box
+  	let fileName = document.getElementById("userInput").value.trim();
+  	// Store in a variable
+	if (!fileName) {
+    		alert("Please enter a file name.");
+    		return;
+  	}
+	// Load StudentDatabase.json
+	const response = await fetch("StudentDatabase.json");
+	const students = await response.json();
+}
 // Hosted file case: Use URL-based validation
 fetch("https://html5.validator.nu/?out=json&doc=" + encodeURIComponent(loc), { 
 	method: "GET"
@@ -10,11 +22,5 @@ fetch("https://html5.validator.nu/?out=json&doc=" + encodeURIComponent(loc), {
 		console.warn(error);
 		renderErrorFooter();
 	});
-}
-function saveInput() {
-  // Grab the value from the text box
-  let textValue = document.getElementById("userInput").value;
-  // Store in a variable
-  console.log("You entered:", textValue);
 }
 
