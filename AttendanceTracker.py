4f259a10ed8a2059d86092442e455e0aa640f167
Sean Evans <sean.evans1.2020@gmail.com>
2025-10-21 14:58:55 -0400

    basicDatabase

--- AttendanceTracker.py
+++ AttendanceTracker.py
@@ -1,3 +1,35 @@
-import SQLAlchemy # type: ignore
+import sqlite3
+from sqlalchemy import ( #type: ignore
+    create_engine,
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    ForeignKey,
+)
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship #type: ignore
+
+# Set up the database using SQLAlchemy
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = "students"
+    name = Column(String)
+    studentID = Column(String, primary_key=True)
+
+# Set up SQLite database
+engine = create_engine("sqlite:///students.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+users = Student(name: "bob", studentID: "1") #type: ignore
+session.add(users)
+users = Student(name: "dob", studentID: "2") #type: ignore
+session.add(users)
+users = Student(name: "pob", studentID: "3") #type: ignore
+session.add(users)
+session.commit()
 
 user = str(input("what school username"))
