be78acd04fcde1cce04e4e123fc78b71568fe54a
Sean Evans <sean.evans1.2020@gmail.com>
2026-03-24 08:08:26 -0400

    First5Fix

--- DigitalClock.py
+++ DigitalClock.py
@@ -233,12 +233,10 @@ def is_first_5_mins():
             last_bell_min = bell_min
         else:
             break
-
+    
     # the amount of time we've been in class
-    try:
+    if (time_diff_seconds <= (5*60)):
         time_diff_seconds = (current_seconds - (last_bell_min * 60))
-    except:
-        time_diff_seconds = 0
 
     if time_diff_seconds > 0:
         return time_diff_seconds
@@ -509,7 +507,7 @@ def main():
             continue
         
         # If FirstFive is on run it
-        elif(FIRST_FIVE == True and first_5_mins <= (5*60)):
+        elif(FIRST_FIVE == True and first_5_mins != -1):
                 matrixportal.set_text("First 5:", 0)
                 matrixportal.set_text(str(first_5_mins) + " secs", 1)
                 scroll_speed_update()
