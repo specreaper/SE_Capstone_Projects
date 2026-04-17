022db397b2d563c78a51f24def70f95b5329bcd5
Sean Evans <sean.evans1.2020@gmail.com>
2026-04-17 11:17:36 -0400

    DatabaseTest

--- AttendanceTracker.py
+++ AttendanceTracker.py
@@ -1,3 +1,5 @@
+import os
+import time
 import sqlite3
 from sqlalchemy import ( #type: ignore
     create_engine,
@@ -17,24 +19,28 @@ engine = create_engine(databaseURL)
 
 class Student(Base):
     __tablename__ = "students"
-    name = Column(String)
-    studentID = Column(String, primary_key=True)
+    StudentID = Column(Integer, primary_key=True)
+    # Date = Column(DateTime, primary_key=True)
+    Period = Column(String, primary_key=True)
+    Name = Column(String)
+    # Present = Column(Boolean)
 
 # Set up SQLite database
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-users = Student(name: "bob", studentID: "1") #type: ignore
+users = Student(StudentID: 123, Period: "3A", Name: "Bob") #type: ignore
 session.add(users)
-users = Student(name: "dob", studentID: "2") #type: ignore
+users = Student(StudentID: 456, Period: "3A", Name: "Dob") #type: ignore
 session.add(users)
-users = Student(name: "pob", studentID: "3") #type: ignore
+users = Student(StudentID: 789, Period: "3A", Name: "Pob") #type: ignore
 session.add(users)
 session.commit()
 
-user = str(input("what school username"))
-if(user == session.query(Student).filter_by(studentID = user).first()):
+user = str(input("what Student ID"))
+if(user == session.query(Student).filter_by(studentID = user)):
+    print("working")
     #Log student
 
 session.close() #type: ignore
